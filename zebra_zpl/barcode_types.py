fbb0dd47a0629be9110e2027f2106903a7444423
Michael King <mking@hey.com>
2020-08-05 21:49:42 -0400

    add barcode types w/ default values

---
+++ zebra_zpl/barcode_types.py
@@ -0,0 +1,24 @@
+barcode_types = {
+    '0': {
+        'orientation': 'N',
+        'magnification': 2,
+        'ecic': 'N',
+        'correction_level': 0,
+        'menu_symbol': 'N',
+        'num_symbols': 1
+    },
+    'C': {
+        'orientation': 'N',
+        'height': None,
+        'human_readable': 'N',
+        'print_hr_above': 'N',
+        'check_digit': 'N'
+    },
+    'Q': {
+        'orientation': 'N',
+        'mode': 2,
+        'magnification': 2,
+        'correction_level': 'M',
+        'mask_value': 7
+    }
+}

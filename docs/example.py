45ffc60638814df575d9fe11c7504b1a533e4ecb
Michael King <mking@hey.com>
2020-08-20 21:25:37 -0400

    update example

--- docs/example.py
+++ docs/example.py
@@ -2,34 +2,14 @@ from zebra_zpl import Label, Text, Barcode
 
 label = Label(width=609, length=507.5)
 
-t1 = Text('Hello, ZPL!',
-        font_type=0,
-        font_size=28,
-        width=label.width,
-        y=25,
-        justification='C')
+t1 = Text('Hello, ZPL!', font_type=0, font_size=28,
+          position=(0, 25), width=label.width, y=25, justification='C')
 
 label.add(t1)
 
-bc1 = Barcode('11235813',
-        type='Q',
-        magnification=5,
-        mode='2',
-        position=(100, 50),
-        width=2,
-        height=100,
-        justification='C')
+qr_code = Barcode('11235813', type='Q', magnification=5, mode='2',
+                  position=(label.width/2.5, 25), width=2, height=100)
 
-label.add(bc1)
-
-bc2 = Barcode('HelloBarcode',
-        type='C',
-        human_readable='Y',
-        position=(100, 300),
-        width=2,
-        height=100,
-        justification='C')
-        
-label.add(bc2)
+label.add(qr_code)
 
 label.dump_contents()
